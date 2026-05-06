6c24e9b4c46e97a92904c1e24af3dd8174f9eaf2
Youssefs20 <yousef17453@gmail.com>
2026-05-06 20:02:08 +0300

    fixed board

--- src/board.py
+++ src/board.py
@@ -3,15 +3,14 @@ from square import Square
 
 class Board:
     def __init__(self):
-        self.squares = [[0,0,0,0,0,0,0,0] for col in range(COLS)]
-
+        self.squares: list[list[Square | None]] = \
+            [[None] * COLS for row in range(ROWS)]
         self._create()
 
     def _create(self):
         for row in range(ROWS):
             for col in range(COLS):
-                self.squares[row][col] = Square(row, col)            
-
+                self.squares[row][col] = Square(row, col)
 
     def _add_pieces(self, color):
         pass
