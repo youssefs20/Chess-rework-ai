307c9d3d59cec35a448214e2e83e686bc0abf8f1
youssef sameh <114283304+youssefs20@users.noreply.github.com>
2026-05-12 20:45:35 +0300

    Pieces (#2)
    * added pieces classes
    
    * added pieces to board

--- src/game.py
+++ src/game.py
@@ -1,9 +1,11 @@
 import pygame
 from const import *
+from board import Board
+from piece import *
 
 class Game:
     def __init__(self):
-        pass
+        self.board = Board()
     
     # show methods
 
@@ -18,3 +20,22 @@ class Game:
                 rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
 
                 pygame.draw.rect(surface,color,rect)
+
+
+    def show_pieces(self,surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].piece
+
+                    img = pygame.image.load(piece.texture)
+                    img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
+                    piece.texture_rect = img.get_rect(center=img_center)    
+                    surface.blit(img, piece.texture_rect)  
+
+
+
+
+
+
+
