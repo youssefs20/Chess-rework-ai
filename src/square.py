2a158de33bdb5b98cfb7e05f8da5fc3dee9f62f4
Youssefs20 <yousef17453@gmail.com>
2026-05-22 18:01:55 +0300

    finished legal moves

--- src/square.py
+++ src/square.py
@@ -7,3 +7,26 @@ class Square:
 
     def has_piece(self):
         return self.piece is not None
+    
+
+    def isEmptyOrRival(self, color):
+        return not self.has_piece() or self.piece.color != color
+    
+    def hasRivalPiece(self,color):
+        return self.has_piece() and self.piece.color != color 
+    
+    def isEmpty(self):
+        return not self.has_piece()
+    
+    def has_team_piece(self, color):
+        return self.has_piece() and self.piece.color == color
+
+    
+    #static method is going to be used in the board class to check if a move is within the board limits
+    # we can use it in the game class to check if a move is valid before making it
+    @staticmethod
+    def in_range(*args):
+        for arg in args:
+            if arg < 0 or arg > 7 :
+                return False
+        return True
